071928c0e1fb180f426dd7856ef97cc3f425019c
Miroslav Bajtoš <miroslav@strongloop.com>
2015-08-10 16:27:19 +0200

    Disable tests on Jenkins slaves w/o browser

--- test/e2e/driver.js
+++ test/e2e/driver.js
@@ -3,6 +3,14 @@
 */
 'use strict';
 
+// running on CI, but DISPLAY is NOT provided
+if (process.env.CI && !process.env.DISPLAY) {
+  // skip browser tests
+  console.error('Running on CI but DISPLAY is NOT provided.');
+  console.error('Skipping all browser tests.');
+  process.exit(0);
+}
+
 var webdriver = require('selenium-webdriver');
 
 var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.firefox()).build();
